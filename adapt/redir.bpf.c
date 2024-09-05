780638efada07418c14fdeeef1381fb527a5e237
Tim Baer <tbaer@aviatrix.com>
2024-09-05 06:11:37 +0000

    implement sticky parameter

--- adapt/redir.bpf.c
+++ adapt/redir.bpf.c
@@ -1,6 +1,7 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 
+#include <arpa/inet.h>
 #include <linux/if_ether.h>
 #include <linux/in.h>
 #include <linux/ip.h>
@@ -9,9 +10,11 @@
 
 #define DEBUG 1
 #define MAX_INTFS 16
+#define MAX_FLOWS 32
 #define AF21_HEX 0x48
 #define ENS5_INTF_IDX 2
 #define UINT32_MAX 0xFFFFFFFF
+#define STICKY 100000
 
 char _license[] SEC("license") = "GPL";
 
@@ -39,8 +42,24 @@ struct {
   __uint(flags, BPF_F_MMAPABLE);
 } intf_latency SEC(".maps");
 
+struct flow {
+  __u32 src_ip;
+  __u16 src_port;
+  __u32 dst_ip;
+  __u16 dst_port;
+  __u8 proto;
+};
+
+struct {
+  __uint(type, BPF_MAP_TYPE_HASH);
+  __type(key, struct flow);
+  __type(value, __u32);
+  __uint(max_entries, MAX_FLOWS);
+  __uint(flags, BPF_F_MMAPABLE);
+} flow_intf SEC(".maps");
+
 // Calling the BPF helper inline cannot be unrolled on clang 14.
-struct latency_ns *lookup_latency(int k) {
+struct latency_ns *lookup_latency(__u32 k) {
   return bpf_map_lookup_elem(&intf_latency, &k);
 }
 
@@ -48,13 +67,13 @@ SEC("tc")
 int adapt_redir(struct __sk_buff *skb) {
   // Find IP header.
   void *data = (void *)(long)skb->data;
-  struct ethhdr *eth = data;
+  const struct ethhdr *eth = data;
   void *data_end = (void *)(long)skb->data_end;
 
   if (data + sizeof(*eth) > data_end)
     goto cleanup;
 
-  struct iphdr *iph = data + sizeof(*eth);
+  const struct iphdr *iph = data + sizeof(*eth);
 
   if (iph + 1 > data_end)
     goto cleanup;
@@ -68,7 +87,7 @@ int adapt_redir(struct __sk_buff *skb) {
     goto cleanup;
 
   // Find TCP header.
-  struct tcphdr *tcph = (struct tcphdr *)((__u32 *)iph + iph->ihl);
+  const struct tcphdr *tcph = (struct tcphdr *)((__u32 *)iph + iph->ihl);
   if (tcph + 1 > data_end)
     goto cleanup;
 
@@ -105,7 +124,6 @@ int adapt_redir(struct __sk_buff *skb) {
 #endif
 
   // Adaptive routing algorithm.
-  // TODO(TIM): add sticky hyperparameter.
   __u32 best_idx = ENS5_INTF_IDX;
   __u32 best_avg = UINT32_MAX;
   __u32 meets_dead = 0;
@@ -120,6 +138,7 @@ int adapt_redir(struct __sk_buff *skb) {
     bpf_printk("max intf latency (ns): %lu\n", v->max);
     bpf_printk("avg intf latency (ns): %lu\n", v->avg);
 #endif
+    // Select interface that meets deadline with best average latency.
     if (meets_dead) {
       if (v->max + now_ns < dead_ns && v->avg < best_avg) {
         best_idx = k;
@@ -131,11 +150,53 @@ int adapt_redir(struct __sk_buff *skb) {
       best_avg = v->avg;
     }
   }
+#ifdef DEBUG
+  bpf_printk("best interface: %d\n", best_idx);
+#endif
+
+  // Prefer to stick flow to previous interface if only slightly worse.
+  struct flow f = {iph->saddr, tcph->source, iph->daddr, tcph->dest,
+                   iph->protocol};
+#ifdef DEBUG
+  bpf_printk("flow src ip: %lu\n", f.src_ip);
+  bpf_printk("flow src port: %u\n", htons(f.src_port));
+  bpf_printk("flow dst ip: %lu\n", f.dst_ip);
+  bpf_printk("flow dst port: %d\n", htons(f.dst_port));
+  bpf_printk("flow protocol: %u\n", f.proto);
+#endif
+  __u32 *prev_idx;
+  prev_idx = bpf_map_lookup_elem(&flow_intf, &f);
+#ifdef DEBUG
+  if (prev_idx)
+    bpf_printk("previous interface found: %d\n", *prev_idx);
+  else
+    bpf_printk("previous interface not found\n");
+#endif
+  if (prev_idx && *prev_idx != best_idx) {
+    struct latency_ns *v = lookup_latency(*prev_idx);
+    if (v && meets_dead) {
+      if (v->max + now_ns < dead_ns && v->avg < STICKY + best_avg) {
+        best_idx = *prev_idx;
+#ifdef DEBUG
+        bpf_printk("sticking to previous interface: %d\n", *prev_idx);
+#endif
+      }
+    } else if (v && v->avg < STICKY + best_avg) {
+      best_idx = *prev_idx;
+#ifdef DEBUG
+      bpf_printk("sticking to previous interface: %d\n", *prev_idx);
+#endif
+    }
+  }
+
+  if (bpf_map_update_elem(&flow_intf, &f, &best_idx, BPF_ANY))
+    goto cleanup;
 
 #ifdef DEBUG
-  if (!meets_dead)
+  if (meets_dead)
+    bpf_printk("interface meets deadline\n");
+  else
     bpf_printk("no interface can meet deadline\n");
-  bpf_printk("routing through best interface: %d\n", best_idx);
 #endif
   return bpf_redirect_neigh(best_idx, NULL, 0, 0);
 
